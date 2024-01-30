61a62436e0cbb2233d720a6f8cf921fe89e47158
Emma Lorensson <emma.lorensson@outlook.com>
2024-01-30 14:09:37 +0100

    Sortering på kurskod och kursnamn ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,78 @@
 "use strict";
 
+const url = "https://dahlgren.miun.se/ramschema_ht23.php";
+
+
 async function loadCourses() {
-    try {
-        const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
-        const data = await response.json();
-        const tbody = document.getElementById('tbody');
+  try {
+    //Fetch-anrop
+    const response = await fetch(url);
+    const data = await response.json();
 
-       //Loopa igenom och skriv ut till DOM
+    dataToTable(data);
 
-        for (let i = 0; i < data.length; i++){
-            let row = `<tr>
-            <td>${data[i].code}</td>
-            <td>${data[i].coursename}</td>
-            <td>${data[i].progression}</td>
-            </tr>`
+    //Hämta in element som ska vara klickbara
+    const thCodeEl = document.getElementById('th-code');
+    const thNameEl = document.getElementById('th-name');
+    const thProgressionEl = document.getElementById('th-progression')
 
-            tbody.innerHTML += row
-        }
+    //Eventlisteners
+    thCodeEl.addEventListener("click", function() {
+        emptyTable();
+        data.sort((a, b) => (a.code > b.code) ? 1 : -1);
+        dataToTable(data);
+    });
 
-    } catch {
-        document.getElementById("table").innerHTML = "<p>Kunde inte ladda tabell...</p>"
-    }
+
+    thNameEl.addEventListener("click", function() {
+        emptyTable();
+        data.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+        dataToTable(data);
+    });
+
+    thProgressionEl.addEventListener("click", function() {
+        emptyTable();
+        data.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+        dataToTable(data);
+    });
+
+  } catch {
+    document.getElementById("error").innerHTML =
+      "<p>Kunde inte ladda tabell...</p>";
+  }
 }
-
 
 loadCourses();
 
 
+function dataToTable(data) {
+    const tBody = document.getElementById("tbody");
 
-//För varje kurs/objekt skriv ut till tabell
+    //Loopa igenom och skriv ut kurser till DOM
+    for (let i = 0; i < data.length; i++) {
+      let row = `
+      <tr>
+            <td>${data[i].code}</td>
+            <td>${data[i].coursename}</td>
+            <td>${data[i].progression}</td>
+    </tr>
+    `;
+
+      tBody.innerHTML += row;
+    }
+}
+
+function emptyTable() {
+    const tBody = document.getElementById("tbody");
+    tBody.innerHTML = "";
+}
+
+
+
+
 
 //Vid klick på kurskod, sortera tabell
 
 //Vid klick på Namn, sortera namn
 
 //Vid klick på progression, sortera progression
-
